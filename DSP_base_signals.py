6cbbf8692fe0a198d7ba520cdddf12b78ac3bd2a
Vadzim Herasimovich <vadim.gerasimovich@gmail.com>
2020-04-16 13:27:27 +0300

    DSP_base_signals: add cosine signal generator realization.

--- DSP_base_signals.py
+++ DSP_base_signals.py
@@ -21,10 +21,28 @@ def fading_exponent_generator(signal_length=None, fading_speed=None):
     assert fading_speed is not None, "Fading parameter is not specified!"
     assert fading_speed < 1, "Fading parameter is wrong!"
     unit_step = unit_step_function_generator(signal_length)
-    fading_exponent_signal = [(fading_speed ** n)*unit_step[n] for n in range(signal_length)]
+    fading_exponent_signal = [(fading_speed ** n) * unit_step[n] for n in range(signal_length)]
     return fading_exponent_signal
 
 
-fading_exponent = fading_exponent_generator(signal_length=10, fading_speed=0.7)
-plt.stem(fading_exponent)
+def cosine_generator(signal_length=None, **kwargs):
+    assert signal_length is not None, "Signal length is not specified!"
+    magnitude = 1
+    phase0 = 0
+    frequency = math.pi
+    for key in kwargs:
+        if key == 'Magnitude':
+            magnitude = kwargs[key]
+        elif key == 'init_phase':
+            phase0 = kwargs[key]
+        elif key == 'frequency':
+            frequency = kwargs[key]
+    cos_signal = [magnitude * math.cos(frequency * n + phase0) for n in range(signal_length)]
+    return cos_signal
+
+
+cosine_parameters = {'Magnitude': 1,
+                     'frequency': 2 * math.pi / 16}
+cosine_signal = cosine_generator(signal_length=15, **cosine_parameters)
+plt.stem(cosine_signal)
 plt.show()
