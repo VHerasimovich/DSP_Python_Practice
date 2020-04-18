2bf56bfcb1c6135f7aa5f504256f206e9bcdbed3
Vadzim Herasimovich <vadim.gerasimovich@gmail.com>
2020-04-18 12:32:38 +0300

    difference_equation: add read coefficients from file.

--- difference_equations.py
+++ difference_equations.py
@@ -3,12 +3,12 @@ import DSP_base_signals as bs
 
 
 def difference_equation(input_signal, *coefficients):
-    signal_length = len(input_signal)
-    coeffs_b = coefficients[0]
-    coeffs_a = coefficients[1]
-    b_length = len(coeffs_b)
-    a_length = len(coeffs_a)
-    for signal_sample in range(signal_length):
+    work_signal_length = len(input_signal)
+    coefficients_b = coefficients[0]
+    coefficients_a = coefficients[1]
+    b_length = len(coefficients_b)
+    a_length = len(coefficients_a)
+    for signal_sample in range(work_signal_length):
         a_part, b_part = 0, 0
         for i in range(b_length):
             b_part += coefficients_b[i] * input_signal[signal_sample - i]
@@ -22,11 +22,18 @@ signal_length = 64
 delta_signal = bs.delta_signal_generator(signal_length, 0)
 # unit_step_signal = bs.unit_step_function_generator(signal_length)
 output_signal = [0 * j for j in range(signal_length)]    # initialization with zeros
-# a and b filter coefficients
-coefficients_b = (0.67549, 0.05196, 1.35097)
-coefficients_a = (1, 0.06797, 1.53449)
+# all filter coefficients are in the file: first string -- b coeffs, second string -- a coeffs
+filename = 'filter_coefficients'
+file = open(filename, 'r')
+file_data = file.read()
+i = 0
+coefficients_string = file_data.split('\n', 2)
+coefficients_string[0] = coefficients_string[0].split(' ')
+coefficients_string[1] = coefficients_string[1].split(' ')
+coefficients_b_tuple = tuple([float(value) for value in coefficients_string[0]])
+coefficients_a_tuple = tuple([float(value) for value in coefficients_string[1]])
 
-filtered_signal = difference_equation(delta_signal, *(coefficients_b, coefficients_a))
+filtered_signal = difference_equation(delta_signal, *(coefficients_b_tuple, coefficients_a_tuple))
 
 plt.stem(output_signal)
 plt.grid(True)
