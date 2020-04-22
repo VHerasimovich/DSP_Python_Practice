f22847a8c166e88d9b602f1e26678de896cac5fd
Vadzim Herasimovich <vadim.gerasimovich@gmail.com>
2020-04-22 19:59:46 +0300

    difference_equation: make magnitude and phase response in separate function.

--- difference_equations.py
+++ difference_equations.py
@@ -6,6 +6,7 @@ import cmath
 
 def difference_equation(input_signal, *coefficients):
     work_signal_length = len(input_signal)
+    output_signal = [0 * j for j in range(work_signal_length)]  # initialization with zeros
     coefficients_b = coefficients[0]
     coefficients_a = coefficients[1]
     b_length = len(coefficients_b)
@@ -20,10 +21,28 @@ def difference_equation(input_signal, *coefficients):
     return output_signal
 
 
-signal_length = 64
-delta_signal = bs.delta_signal_generator(signal_length, 0)
-# unit_step_signal = bs.unit_step_function_generator(signal_length)
-output_signal = [0 * j for j in range(signal_length)]    # initialization with zeros
+def magnitude_phase_response(frequency_response_length, *coefficients):
+    impulse_response = difference_equation(bs.delta_signal_generator(frequency_response_length, 0), *coefficients)
+    frequency_response = [0 * n for n in range(frequency_response_length)]
+    magnitude_response = [0 * n for n in range(frequency_response_length)]
+    phase_response = [0 * n for n in range(frequency_response_length)]
+    omega = 0   # frequency dots for response evaluation
+    for n in range(frequency_response_length):
+        omega += cmath.pi / frequency_response_length
+        for m in range(frequency_response_length):
+            frequency_response[n] += impulse_response[m] * cmath.exp(1j * omega * m)
+        magnitude_response[n] = 20*math.log10(math.sqrt(frequency_response[n].real ** 2 + frequency_response[n].imag ** 2))
+        phase_response[n] = math.atan2(-frequency_response[n].imag, frequency_response[n].real)
+        # Matlab's unwrap func analogue: it allows to cross over [-pi +pi] atan2 limit
+        if phase_response[n] - phase_response[n-1] > math.pi:
+            while phase_response[n] - phase_response[n-1] > math.pi:
+                phase_response[n] -= 2*math.pi
+    # radians to degree conversion
+    for n in range(frequency_response_length):
+        phase_response[n] *= 180/math.pi
+
+    return magnitude_response, phase_response
+
 
 # all filter coefficients are in the file: first string -- b coeffs, second string -- a coeffs
 filename = 'filter_coefficients'
@@ -45,29 +64,36 @@ finally:
     if file is not None:
         file.close()
 
+signal_length = 64
+delta_signal = bs.delta_signal_generator(signal_length, 0)
 filtered_signal = difference_equation(delta_signal, *(coefficients_b_tuple, coefficients_a_tuple))
 
 # plt.stem(filtered_signal)
 # plt.grid(True)
 # plt.show()
 
-frequency_response = [0 * n for n in range(signal_length)]
-magnitude_response = [0 * n for n in range(signal_length)]
-magnitude_response = [0 * n for n in range(signal_length)]
-phase_response = [0 * n for n in range(signal_length)]
-omega = 0
-frequency_response_length = 64
-for n in range(signal_length):
-    omega += cmath.pi / signal_length
-    for m in range(frequency_response_length):
-        frequency_response[n] += filtered_signal[m] * cmath.exp(1j*omega*m)
-    magnitude_response[n] = math.sqrt(frequency_response[n].real ** 2 + frequency_response[n].imag ** 2)
-    phase_response[n] = math.atan2(-1*frequency_response[n].imag, frequency_response[n].real) * (180/math.pi)
+response_length = 512
+(filter_magnitude_response, filter_phase_response) = \
+    magnitude_phase_response(response_length, *(coefficients_b_tuple, coefficients_a_tuple))
+
+# x axis dots of normalized frequency
+x_axis = [i/response_length for i in range(response_length)]
 
 plt.subplot(2, 1, 1)
-plt.plot(magnitude_response)
+plt.plot(x_axis, filter_magnitude_response)
+plt.xlabel("Normalized frequency")
+plt.ylabel("Magnitude, dB")
 plt.grid(True)
+plt.xlim(0, max(x_axis))
+
+plt.subplots_adjust(hspace=0.3, top=0.95)
+# hspace -- space between axes, top -- space from top to axes
+
 plt.subplot(2, 1, 2)
-plt.plot(phase_response)
+plt.plot(x_axis, filter_phase_response)
+plt.ylabel("Phase, degree")
+plt.xlabel("Normalized frequency")
 plt.grid(True)
+plt.xlim(0, max(x_axis))
+
 plt.show()
